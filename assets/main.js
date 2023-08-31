da316ef0737a32862438dfc96fed9dcbb4664493
Valentina De Mite <valesakura97@gmail.com>
2023-08-31 18:22:18 +0200

    complete pari e dispari

--- assets/main.js
+++ assets/main.js
@@ -15,25 +15,36 @@ document.getElementById("inserisci").addEventListener("click", function () {
   const userNumber = document.getElementById("user_number").value;
   console.log("Il numero che hai scelto e': " + userNumber);
 
-  if (userNumber > 5 && userNumber == 0) {
+  if (userNumber > 5) {
     alert("Errore, scegli un numero da 1 a 5");
+    location.reload();
   }
 
-  document.getElementById;
-  function isEven(userNumber) {
-    if (userNumber % 2 === 0) {
-      return true;
-    }
-    return false;
+  let choice = document.getElementById("choice").value;
+  const even = document.getElementById("even");
+  const odd = document.getElementById("odd");
+
+  if (choice == "even") {
+    console.log("hai scelto un numero pari");
+  }
+  if (choice === "odd") {
+    console.log("hai scelto un numero dispari");
   }
 
-  console.log(isEven(userNumber));
-  const result = isEven(userNumber);
-  console.log(result);
-  if (isEven(userNumber)) {
-    console.log("il numero e' pari");
-  } else {
-    console.log("il numero e' dispari");
+  Math.floor(Math.random() * 10 + 1);
+
+  let pcNumber = Math.floor(Math.random() * 5 + 1);
+  console.log("il numero del PC e' " + pcNumber);
+
+  let sum = userNumber + pcNumber;
+  console.log("la somma dei due numeri e': " + sum);
+
+  if (sum % 2 == 0) {
+    message = "Hai vinto tu!";
   }
-  isEven(userNumber);
+  if (sum % 2 != 0) {
+    message = "Ha vinto il PC!";
+    console.log("");
+  }
+  document.getElementById("winner").innerHTML = message;
 });
